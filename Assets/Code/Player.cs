a48fc58a1cd8b7d48a0785df8325a4712a97af8e
jaredh01 <jaredhatcher2020@u.northwestern.edu>
2019-02-05 19:06:59 -0600

    Adding compatibility for multiple players
    Adding editor variable PlayerNumber, AssignPlayerColor

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int PlayerNumber;
+
+    private Rigidbody2D _rb;
+    private SpriteRenderer _spriteRenderer;
+
+    internal void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        AssignPlayerColor();
     }
 
-    // Update is called once per frame
-    void Update()
+    internal void Update()
     {
-        
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        // Placeholder 
+        _rb.velocity += Vector2.right * Input.GetAxis( "Horizontal" + PlayerNumber );
+    }
+
+    /// <summary>
+    /// Modifies the player's <see cref="SpriteRenderer.color"/>, based on <see cref="PlayerNumber"/>
+    /// </summary>
+    private void AssignPlayerColor()
+    {
+        Color playerColor;
+        switch (PlayerNumber)
+        {
+            case 1:
+                playerColor = Color.red;
+                break;
+            case 2:
+                playerColor = Color.blue;
+                break;
+            case 3:
+                playerColor = Color.yellow;
+                break;
+            case 4:
+                playerColor = Color.green;
+                break;
+            default:
+                playerColor = Color.white;
+                break;
+        }
+
+        _spriteRenderer.color = playerColor;
     }
 }
