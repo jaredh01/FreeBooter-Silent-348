6fb4053fd7f6363e6a5a5624720a06f46bbeb4c7
jaredh01 <jaredhatcher2020@u.northwestern.edu>
2019-02-15 20:31:44 -0600

    Very ugly but functional scoring UI

--- Assets/Code/ScoreManager.cs
+++ Assets/Code/ScoreManager.cs
@@ -1,35 +1,54 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    private List<Text> _scoreTexts;
+    public int NumberOfPlayers;
+
+    private GameObject _scoreSection;
+    private List<TextMeshProUGUI> _scoreTexts = new List<TextMeshProUGUI>();
     private int[] _scores = new int[4];
 
     // Start is called before the first frame update
     void Start()
     {
-        _scoreTexts.Add( GameObject.Find( "Score1Text" ).GetComponent<Text>() );
-        _scoreTexts.Add( GameObject.Find( "Score2Text" ).GetComponent<Text>() );
-        _scoreTexts.Add( GameObject.Find( "Score3Text" ).GetComponent<Text>() );
-        _scoreTexts.Add( GameObject.Find( "Score4Text" ).GetComponent<Text>() );
-        Array.Clear(_scores, 0, 4);
+        _scoreSection = GameObject.FindGameObjectWithTag( "Score" );
+        ActivatePlayerScores();
+
+        Array.Clear( _scores, 0, 4 );
     }
 
     public void ScorePoints( int score, int playerNumber )
     {
-        _scores[playerNumber] += score;
+        _scores[playerNumber - 1] += score;
         UpdateScores();
     }
 
     private void UpdateScores()
     {
-        for ( var i = 0; i < 4; i++ )
+        for ( var i = 0; i < NumberOfPlayers - 1; i++ )
         {
             _scoreTexts[i].text = "Score: " + _scores[i];
+        }
+    }
+
+    private void ActivatePlayerScores()
+    {
+        foreach ( Transform section in _scoreSection.transform )
+        {
+            var sectionNumber = section.gameObject.name[section.gameObject.name.Length - 1] - '0';
+            if ( sectionNumber <= NumberOfPlayers )
+            {
+                _scoreTexts.Add( GameObject.Find("Player " + sectionNumber + " Score Text").GetComponent<TextMeshProUGUI>() );
+            }
+            else
+            {
+                section.gameObject.SetActive( false );
+            }
         }
     }
 }
