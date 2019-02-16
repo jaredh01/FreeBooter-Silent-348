3ec333a7a087d9e4851a0ddd872f7a381d88f593
jaredh01 <jaredhatcher2020@u.northwestern.edu>
2019-02-15 21:40:43 -0600

    Main Menu

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Code
+{
+    public class MainMenu : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var startButtons = GameObject.Find( "Choices/StartButtons" );
+            foreach ( Transform buttonObject in startButtons.transform )
+            {
+                var playerCount = buttonObject.gameObject.name[buttonObject.gameObject.name.Length - 2] - '0';
+                buttonObject.gameObject.GetComponent<Button>().onClick.AddListener( () => StartGame( playerCount ) );
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        private void StartGame( int playerCount )
+        {
+            GameConfig.NumberOfPlayers = playerCount;
+            SceneManager.LoadScene( "MainScene" );
+        }
+    }
+}
