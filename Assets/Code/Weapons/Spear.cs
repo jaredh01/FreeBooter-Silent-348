c32532921a0719d41fb2de2363243b4027d277b8
jaredh01 <jaredhatcher2020@u.northwestern.edu>
2019-02-17 23:56:10 -0600

    Cooldown animation to nerf spears

--- Assets/Code/Weapons/Spear.cs
+++ Assets/Code/Weapons/Spear.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Spear : Weapon
 {
     public float AttackLength = 0.5f;
+    public float CoolDownLength = 0.4f;
 
     private float _attackTimer;
+    private float _coolDownTimer;
 
     public override void UseWeapon()
     {
-        if (_isActive || !IsCarried) return;
+        if (_isActive || !IsCarried || _coolDownTimer > 0 ) return;
         _isActive = true;
         _attackTimer = AttackLength;
+        _coolDownTimer = CoolDownLength;
         _audioSource.Play();
         gameObject.transform.Rotate(0, 0, -90);
         StartCoroutine( "SpearAttack" );
@@ -22,16 +25,17 @@ public class Spear : Weapon
     {
         _isActive = false;
         gameObject.transform.localPosition = Vector3.zero;
-        gameObject.transform.Rotate(0, 0, 90);
     }
 
     public override void DropWeapon()
     {
-        if ( _isActive )
+        if ( _isActive || _coolDownTimer > 0)
         {
             StopCoroutine( "SpearAttack");
             UnuseWeapon();
+            _coolDownTimer = 0;
         }
+        gameObject.transform.localRotation = Quaternion.Euler(Vector3.zero);
         IsCarried = false;
         gameObject.transform.position = CarryingPlayer.transform.position + new Vector3( 0, 1, 0 );
         CarryingPlayer = null;
@@ -40,11 +44,25 @@ public class Spear : Weapon
 
     private IEnumerator SpearAttack()
     {
+        // Stab forward
         while ( _attackTimer > 0 )
         {
             _attackTimer -= Time.fixedDeltaTime;
             var lerpVar = ( AttackLength - _attackTimer ) / AttackLength;
             gameObject.transform.localPosition = Vector3.Slerp( 0.75f * Vector3.left, Vector3.right, lerpVar );
+            yield return null;
+        }
+
+        _isActive = false;
+
+        // Cooldown, reset rotation
+        while (_coolDownTimer > 0)
+        {
+            _coolDownTimer -= Time.fixedDeltaTime;
+            var lerpVar = (CoolDownLength - _coolDownTimer) / CoolDownLength;
+            gameObject.transform.localRotation = Quaternion.Slerp(Quaternion.Euler(new Vector3(0, 0, -90)),
+                Quaternion.Euler(Vector3.zero), lerpVar);
+            gameObject.transform.localPosition = Vector3.Slerp(Vector3.right, Vector3.zero,  lerpVar);
             yield return null;
         }
 
