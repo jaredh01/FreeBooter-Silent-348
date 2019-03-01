96f3c7d46b63aa3e4e692441356cdd857c4e2fc8
LJGladic <lukasgladic2017@u.northwestern.edu>
2019-03-01 13:09:30 -0600

    Start of ScoringZone
    Made the scoring Zone file and started work.  Tagged players as 'player'

---
+++ Assets/Code/ScoringZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringZone : MonoBehaviour
+{
+    private float _maxXPosition;
+    private float _maxYPosition;
+    private float _timeTillMove;
+    private static float _scoringInterval = 1f;
+    private static float _movementTime = 15f;
+
+
+    void Start()
+    {
+        _timeTillMove = _movementTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
