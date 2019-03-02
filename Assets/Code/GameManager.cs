6ad210d2315a86c62c315bd7a4ded720146a382d
jaredh01 <jaredhatcher2020@u.northwestern.edu>
2019-03-02 00:31:40 -0600

    First To X Score

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -16,10 +16,8 @@ public class GameManager : MonoBehaviour
 
 
     private TextMeshProUGUI _statusText;
-    private TextMeshProUGUI _timeText;
     private Camera _camera;
-    private float _timeRemaining;
-    private float _startGameDelay = 5f;
+    private float _startGameDelay = 5.1f;
 
 
     // Currently, start a round immediately
@@ -27,7 +25,7 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 0;
         _camera = FindObjectOfType<Camera>();
-        SetGameTimer();
+        _statusText = GameObject.FindGameObjectWithTag("Status").GetComponent<TextMeshProUGUI>();
         SetActivePlayers();
         StartCoroutine(StartGameAnimation());
     }
@@ -57,24 +55,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
-    {
-        _timeRemaining -= Time.fixedDeltaTime;
-        _timeText.text = "Time Left: " + string.Format("{0}:{1:00}", (int)_timeRemaining / 60, (int)_timeRemaining % 60);
-        if ( _timeRemaining < 0 ) GameOver();
-    }
-
-    /// <summary>
-    /// Helper function for configuring the game timer
-    /// </summary>
-    private void SetGameTimer()
-    {
-        _timeRemaining = RoundLength;
-        _statusText = GameObject.FindGameObjectWithTag("Status").GetComponent<TextMeshProUGUI>();
-        _timeText = GameObject.FindGameObjectWithTag("Time").GetComponent<TextMeshProUGUI>();
-        _timeText.text = "Time Left: " + string.Format("{0}:{1:00}", (int)_timeRemaining / 60, (int)_timeRemaining % 60);
-    }
-
     /// <summary>
     /// Deactivate the game object of players that shouldn't be active
     /// </summary>
@@ -95,15 +75,8 @@ public class GameManager : MonoBehaviour
 
         while ( _startGameDelay > 0f )
         {
-            _statusText.text = string.Format("Starting in: {0}", Mathf.Ceil(_startGameDelay));
+            _statusText.text = string.Format("Starting in: {0}", (int) _startGameDelay);
             _startGameDelay -= Time.unscaledDeltaTime;
-
-
-            if ( _startGameDelay > 4f  || _startGameDelay < 1f ) yield return null;
-            var slerpVar = ( 4f - _startGameDelay ) / 3f;
-            var originPoint = _camera.ViewportToScreenPoint( new Vector3( 0.5f, 0.5f) );
-            var destPoint = _camera.ViewportToScreenPoint( new Vector3( 0.78f, 0.8f ) );
-            _timeText.transform.position =  Vector3.Slerp( originPoint, destPoint, slerpVar  );
             yield return null;
         }
 
@@ -128,7 +101,7 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// Freezes the game when time runs out
     /// </summary>
-    private void GameOver()
+    public void GameOver()
     {
         Time.timeScale = 0;
         GameIsOver = true;
