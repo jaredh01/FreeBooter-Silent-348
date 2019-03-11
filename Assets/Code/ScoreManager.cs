b1dbfddb05eb31b437ff05ea9c4c5db1361a0444
jaredh01 <jaredhatcher2020@u.northwestern.edu>
2019-03-11 17:35:43 -0500

    leader has crown

--- Assets/Code/ScoreManager.cs
+++ Assets/Code/ScoreManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,6 +13,7 @@ public class ScoreManager : MonoBehaviour
     private GameObject _scoreSection;
     private List<TextMeshProUGUI> _scoreTexts = new List<TextMeshProUGUI>();
     private int[] _scores = new int[4];
+    private Player _winningPlayer;
 
     // Start is called before the first frame update
     void Start()
@@ -26,10 +28,34 @@ public class ScoreManager : MonoBehaviour
     {
         _scores[playerNumber - 1] += score;
         UpdateScores();
+        var highestScore = 0;
+        var winningPlayerNumber = 0;
         if (_scores[playerNumber - 1] >= VictoryScore)
         {
             FindObjectOfType<GameManager>().GameOver();
         }
+
+        foreach ( var s in _scores )
+        {
+            if ( s > highestScore )
+            {
+                highestScore = s;
+                winningPlayerNumber = _scores.ToList().IndexOf( highestScore ) + 1;
+            }
+            else if ( s == highestScore )
+            {
+                winningPlayerNumber = 0;
+            }
+        }
+
+        if (_winningPlayer != null && winningPlayerNumber != _winningPlayer.PlayerNumber )
+        {
+            _winningPlayer.transform.Find( "Crown" ).gameObject.SetActive( false );
+        }
+
+        if ( winningPlayerNumber == 0 ) return;
+        _winningPlayer = GameObject.Find( "Player" + winningPlayerNumber ).GetComponent<Player>();
+        _winningPlayer.transform.Find( "Crown" ).gameObject.SetActive( true );
     }
 
     /// <summary>
